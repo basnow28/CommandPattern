772ceb87c8dd3076350a56f5d37e27bd918fdbc5
Barbara Rekowska <basnow28@gmail.com>
2020-09-10 11:18:52 +0200

    Adding execution to the Button

--- src/kea/Button.java
+++ src/kea/Button.java
@@ -4,5 +4,11 @@ public class Button
 {
     private Command command;
 
+    public Button(Command command){
+        this.command = command;
+    }
 
+    public void execute(){
+        this.command.execute();
+    }
 }
